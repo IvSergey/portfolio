ca50dab49e3446ea3c9e109a0b3d29975df8642d
IvSergey <imas.spb@mail.ru>
2018-01-31 00:38:22 +0300

    Initial Commit

---
+++ main.js
@@ -0,0 +1,61 @@
+ymaps.ready(init);
+
+var placemarks = [
+	{
+		latitude: 59.97,
+		longitude: 30.31,
+		
+
+	},
+	{
+		latitude: 59.94,
+		longitude: 30.38,
+		
+	},
+	{
+		latitude: 59.87,
+		longitude: 30.31,
+		
+	},
+	{
+		latitude: 59.91,
+		longitude: 30.48,
+		
+	}
+];
+	
+
+
+function init() {
+	var map = new ymaps.Map('map',{
+		center: [59.94, 30.32],
+		zoom: 11,
+		controls:['zoomControl'],
+		behaviors:['drag']
+		
+	});
+
+	placemarks.forEach(function(obj) {
+		var placemark = new ymaps.Placemark([obj.latitude, obj.longitude],{
+
+		    },
+		    {
+		    	iconLayout:'default#image',
+				iconImageHref: 'img/icons/map-marker.svg',
+				iconImageSize:[46,57]
+		    });
+
+		
+		map.geoObjects.add(placemark);
+
+	});
+}
+
+
+
+
+
+
+
+
+	
