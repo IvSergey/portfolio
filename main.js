6506b0ace9a66217b9b0f809b81596999c0f157d
IvSergey <imas.spb@mail.ru>
2018-02-14 11:10:59 +0300

    Initial Commit

--- main.js
+++ main.js
@@ -282,9 +282,9 @@ $(function () {
 });
 
 
-// let openMenu = document.getElementsByClassName('menu__bg');
-// let openText = document.getElementsByClassName('menu__text');
-// for(let i = 0, i < openMenu.length, i++){
-// 	openMenu[i].addEventListener('click', function () {
-// 	console.log('click');	
-// }
+let openMenu = document.getElementsByClassName('menu__bg');
+let openText = document.getElementsByClassName('menu__text');
+for(let i = 0, i < openMenu.length, i++){
+	openMenu[i].addEventListener('click', function () {
+	console.log('click');	
+})
